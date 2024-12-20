ed496977d59133ce0531967d4305820bd56bb3cd
jeremy.rojas <jtan.rojas@gmail.com>
2024-12-20 13:27:51 -0500

    Removed default vercel favicon

--- next.config.js
+++ next.config.js
@@ -3,12 +3,11 @@ const nextConfig = {
   async headers() {
     return [
       {
-        // Match all favicon related files
-        source: '/:file(favicon.ico|favicon-16x16.png|favicon-32x32.png|apple-touch-icon.png|android-chrome-192x192.png|android-chrome-512x512.png|site.webmanifest)',
+        source: '/:path*',
         headers: [
           {
             key: 'Cache-Control',
-            value: 'public, max-age=0, must-revalidate',
+            value: 'public, max-age=31536000, immutable',
           },
         ],
       },
